219c7fdd78451000062015d5d7233e253e01c93a
Daniyar <bazakovd@gmail.com>
2020-09-07 17:53:34 -0400

    linking pages

--- app/controllers/pokemons_controller.rb
+++ app/controllers/pokemons_controller.rb
@@ -8,14 +8,33 @@ class PokemonsController < ApplicationController
     end
 
     def new
+        @pokemon = Pokemon.new
+    end
+
+    def create
+        @pokemon = Pokemon.new(pokemon_params)
+        @pokemon.save
+        redirect_to pokemon_path(@pokemon)
     end
 
     def edit
+        @pokemon = Pokemon.find(params[:id])
     end
 
     def update
+        @pokemon = Pokemon.find(params[:id])
+        @pokemon.update(pokemon_params)
+        redirect_to pokemon_path(@pokemon)
     end
 
     def destroy
+        @pokemon = Pokemon.find(params[:id])
+        @pokemon.destroy
+        redirect_to pokemons_path
+    end
+
+    private
+    def pokemon_params
+        params.require(:pokemon).permit(:Name, :Type_1, :Type_2, :Total, :HP, :Attack, :Defence, :Sp_Atk, :Sp_Def, :Speed, :Generation, :Legendary)
     end
 end
