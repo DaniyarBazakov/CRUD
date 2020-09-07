eb8d7c049014707ce4f8fe5a668325045ffe4b04
Daniyar <bazakovd@gmail.com>
2020-09-07 18:33:03 -0400

    added links

--- app/controllers/pokemons_controller.rb
+++ app/controllers/pokemons_controller.rb
@@ -1,11 +1,10 @@
 class PokemonsController < ApplicationController
+    before_action :find_pokemon, only: [:show, :edit, :update, :destroy]
     def index # all pokemons
         @pokemons = Pokemon.all
     end
 
-    def show
-        @pokemon = Pokemon.find(params[:id])
-    end
+    def show; end
 
     def new
         @pokemon = Pokemon.new
@@ -17,18 +16,14 @@ class PokemonsController < ApplicationController
         redirect_to pokemon_path(@pokemon)
     end
 
-    def edit
-        @pokemon = Pokemon.find(params[:id])
-    end
+    def edit; end
 
     def update
-        @pokemon = Pokemon.find(params[:id])
         @pokemon.update(pokemon_params)
         redirect_to pokemon_path(@pokemon)
     end
 
     def destroy
-        @pokemon = Pokemon.find(params[:id])
         @pokemon.destroy
         redirect_to pokemons_path
     end
@@ -36,5 +31,9 @@ class PokemonsController < ApplicationController
     private
     def pokemon_params
         params.require(:pokemon).permit(:Name, :Type_1, :Type_2, :Total, :HP, :Attack, :Defence, :Sp_Atk, :Sp_Def, :Speed, :Generation, :Legendary)
+    end
+
+    def find_pokemon
+        @pokemon = Pokemon.find(params[:id])
     end
 end
